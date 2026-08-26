fc1b4833a2916c6a6903461b5b85048fa2b2bf93
Lenwr <jeffersonabousaidabalo@gmail.com>
2026-08-26 11:13:18 +0200

    add branded link previews

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: { chunkSizeWarningLimit: 2000 },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const vercelOrigin = env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : ''
+  const appOrigin = (vercelOrigin || env.VITE_APP_PUBLIC_URL || 'http://localhost:5173').replace(/\/$/, '')
+
+  return {
+    plugins: [
+      vue(),
+      {
+        name: 'transport-fomek-social-metadata',
+        transformIndexHtml(html) {
+          return html.replaceAll('__APP_ORIGIN__', appOrigin)
+        },
+      },
+    ],
+    build: { chunkSizeWarningLimit: 2000 },
+  }
 })
